4efc395a42a5ebf7dc6e5c5dcdfd2dd74dff667a
Theo  Paris <theo@theoparis.com>
2026-02-10 14:56:51 -0800

    feat: add flake.nix
    Signed-off-by: Theo Paris <theo@theoparis.com>
    Change-Id: Iee5b27ca085057ce098270ca05952bbc6a6a6964

--- default.nix
+++ default.nix
@@ -1,9 +1,17 @@
-(import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in
-  fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash;
-  }
-) { src = ./.; }).defaultNix
+{
+  craneLib,
+  self,
+}:
+
+craneLib.buildPackage {
+  pname = "rix";
+  version = "0.1.0";
+  src = craneLib.cleanCargoSource self;
+
+  nativeBuildInputs = [
+  ];
+
+  buildInputs = [
+
+  ];
+}
