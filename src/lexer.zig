710c88f145f8d2762cfdd97c1c9b42c39f77f067
Theo Paris <theo@theoparis.com>
2026-07-25 00:30:39 -0700

    chore: update to 0.17

--- src/lexer.zig
+++ src/lexer.zig
@@ -111,7 +111,7 @@ pub const Lexer = struct {
             .string_depth = 0,
             .brace_depth = 0,
             .allocated_strings = .empty,
-            .indented_string_stack = [_]bool{false} ** max_string_depth,
+            .indented_string_stack = @as([max_string_depth]bool, @splat(false)), 
         };
     }
 
